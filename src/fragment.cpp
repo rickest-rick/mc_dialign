5ffecdb4233555db3596d7b08492d3766d5fd714
Joschi <j.st@wwu.de>
2018-10-23 23:54:42 +0200

    getters, setters and constructor for fragment class

--- src/fragment.cpp
+++ src/fragment.cpp
@@ -3,3 +3,51 @@
 //
 
 #include "fragment.h"
+
+Fragment::Fragment(unsigned int sequence_one_, unsigned int sequence_two_, unsigned int start_point_one_,
+                   unsigned int start_point_two_, unsigned int length_) : sequence_one_(sequence_one_),
+                                                                          sequence_two_(sequence_two_),
+                                                                          start_point_one_(start_point_one_),
+                                                                          start_point_two_(start_point_two_),
+                                                                          length_(length_),
+                                                                          weight_(0) {}
+
+Fragment::Fragment(unsigned int sequence_one_, unsigned int sequence_two_, unsigned int start_point_one_,
+                   unsigned int start_point_two_, unsigned int length_, double weight_) : sequence_one_(sequence_one_),
+                                                                                          sequence_two_(sequence_two_),
+                                                                                          start_point_one_(
+                                                                                                  start_point_one_),
+                                                                                          start_point_two_(
+                                                                                                  start_point_two_),
+                                                                                          length_(length_),
+                                                                                          weight_(weight_) {}
+
+unsigned int Fragment::getSequence_one_() const {
+    return sequence_one_;
+}
+
+unsigned int Fragment::getSequence_two_() const {
+    return sequence_two_;
+}
+
+unsigned int Fragment::getStart_point_one_() const {
+    return start_point_one_;
+}
+
+unsigned int Fragment::getStart_point_two_() const {
+    return start_point_two_;
+}
+
+unsigned int Fragment::getLength_() const {
+    return length_;
+}
+
+double Fragment::getWeight_() const {
+    return weight_;
+}
+
+void Fragment::setWeight_(double weight_) {
+    Fragment::weight_ = weight_;
+}
+
+
--- src/fragment.h
+++ src/fragment.h
@@ -19,7 +19,8 @@
 /**
  * @brief a class representing a diagonal alignment between two sequences
  *
- * Has a start point for each sequence,, a length and a weight which was computed with a weight function.
+ * Has a start point for each sequence, a length and a weight which was computed with a weight function.
+ * All member variables but the weight can be accessed, but not altered.
  *
  * @author Joschka Strueber
  * @version 1.0
@@ -36,7 +37,25 @@ private:
     unsigned double weight_;
 
 public:
+    unsigned int getSequence_one_() const;
 
+    unsigned int getSequence_two_() const;
+
+    unsigned int getStart_point_one_() const;
+
+    unsigned int getStart_point_two_() const;
+
+    unsigned int getLength_() const;
+
+    double getWeight_() const;
+
+    void setWeight_(double weight_);
+
+    Fragment(unsigned int sequence_one_, unsigned int sequence_two_, unsigned int start_point_one_,
+             unsigned int start_point_two_, unsigned int length_);
+
+    Fragment(unsigned int sequence_one_, unsigned int sequence_two_, unsigned int start_point_one_,
+             unsigned int start_point_two_, unsigned int length_, double weight_);
 };
 
 #endif //MC_DIALIGN_FRAGMENT_H
